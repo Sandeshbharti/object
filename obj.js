4c1a03c7a4c05dbc8c61ed921d817d96d23f528e
Sandeshbharti <167829972+Sandeshbharti@users.noreply.github.com>
2024-05-03 20:12:17 +0000

    nextedobject

---
+++ obj.js
@@ -0,0 +1,71 @@
+const student ={
+    fullname : "ram ji ",
+    marks : 75.5,
+    printmark : function(){
+        console.log ("lop something in js ")
+    },
+    lastname : "bharti ",
+}
+//************ access element in obj by (.) dot notation [obj.key =value of key ]
+
+// console.log (student.fullname)
+// console.log (student.marks)
+// student.printmark()
+
+// console.log (student.printmark())
+
+
+//***********prototype(special properties in obj and arr store lot of method )  */
+ 
+const raman={
+    name: "raman raj ",
+    rollno :21 ,
+    adddres : "sahjanwa "
+}
+// ********* set prototype (parent properties accessable in children )
+// here one more method to access (object["key"]) special case 
+let sym  =Symbol ("key1") 
+ let jsuser ={
+    [sym]:"key1",
+    "name" : "hitesh",
+    location : "sahjanwa",
+    email : "h@gamil.com",
+    isLOggedIn : false ,
+    lastlogday : ["monday ","saturaday"]
+}
+
+const symm=Symbol("poo");
+// console.log (symm )
+// console.log (typeof symm) 
+ 
+let newobj={
+    [symm]: "key1"
+}
+// console.log (newobj["symm"])
+// console.log (typeof newobj["symm"])
+//??????????? symbol in object declare not proper working 
+
+
+
+//part 2 
+tinderUser ={}
+tinderUser.Id="123ab"
+tinderUser.name ="sandeshbharti"
+tinderUser.isloggedin = false 
+// console.log (tinderUser)
+
+// we can able to store info in nexted object method 
+ 
+let o={
+    name : "ram ji ji ",
+    addredd: {
+        firstadd: "sahjanwa",
+        secondadd: "gorkhpur",
+        permanentadd: {
+            firstperadd: "vill",
+            secperadd :"city"
+        }
+    }
+}
+//element or info access by nexted dot notation (.)
+
