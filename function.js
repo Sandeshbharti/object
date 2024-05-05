939fe91a99a6c0bb065675f5d2982d70be002906
Sandeshbharti <167829972+Sandeshbharti@users.noreply.github.com>
2024-05-05 05:02:06 +0000

    countvowel and for of loop end

---
+++ function.js
@@ -0,0 +1,104 @@
+//  ***************    function in jawascript    **************
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+//step 1- create function = Functon nameoffuncton (para1,para2,paran){do any work }
+// //step 1- used function or call/invoke the function , [    nameoffunction();    ]
+// function saymyname(num){
+   
+//     if (num>10){
+        
+//             console.log (`that you provide is ${num} greater than 10`)
+//         }
+//         console.log("bharti ")
+//         console.log("sandesh ")
+//         console.log("kumar")
+
+// }
+// saymyname();
+// saymyname(50);
+
+//function for add two number 
+// function addtownum(x,y){
+    // console.log (x+y)}
+
+// addtownum(2,3)
+// addtownum(10,10)
+
+//funtion for mul two num
+// function mul(a,b){
+//     return a*b 
+// } 
+// mul(5,5);
+// console.log (`${mul (5,5)}`) //log as string 
+// console.log (mul(5,5)) //log as a number 
+
+//             ************      Arrow function in js *******************
+ 
+//()=>{},that is the arrow functon ,it need a name(variable) to servive 
+// that is the compact way to write a function ,use it for some compact work 
+
+// let arofunc =(arr)=>{
+// console.log(`that is the arrow function boss ${arr}`)}
+
+
+// arofunc() //if function not get para/argument than set it to undefined 
+// arofunc("sandesh ")
+
+//practice question - create a function that takes a string and 
+// return the count of vowels in the string ?
+
+
+// let count =0;
+// function countfun(string){
+//     for (str of string){ 
+//         if (
+//             str==="a" ||
+//             str==="e" ||
+//             str==="i" ||
+//             str==="o" ||
+//             str==="u"
+//             ){       
+//         count++
+//         }
+//     }
+//     console.log (count)
+// }
+
+// countfun();                   //if para not pass func will give error 
+// countfun("ram");
+// countfun("aeiou");          //second time call karne per privious count  
+// countfun("sdtry");          //second wale main add ho jata hai
+
+// let sun=[10,20,30,40]                          //working exameple(1)
+// sun.forEach((val)=>{                     
+// console.log (val)
+// })
+
+// sun=[10,20,30,40]
+// sun.forEach((val,idx,arr)=>{                     // example(2)
+//     console.log(`${val} ${idx} ${arr}`)
+//    })
+
+
+
+
+//question  cout vowel in string 
+// console.log (" s a n d e s h b h a r  t i ")
+// let total =0;
+// let strin ="mohan"
+// for (str of strin  ){ 
+   
+//             if (
+//                 str==="a"  ||
+//                 str==="e" ||
+//                 str==="i" ||
+//                 str==="o" ||
+//                 str==="u"
+//                 ){
+                
+//             total++
+//             }
+//         }
+//         console.log (total)            
+
+
