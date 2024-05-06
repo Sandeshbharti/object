440ebb6225d93ac1ca8b0c2743457f8ba00dc092
Sandeshbharti <167829972+Sandeshbharti@users.noreply.github.com>
2024-05-06 18:03:54 +0530

    Theone
    Theone

---
+++ sandesh.js
@@ -0,0 +1,96 @@
+// console.log("bharti")
+// console.log("ram ji")
+
+// let color="yellow"
+// if(colour="red"){
+//     console.log(`the color is ${color}`)
+// }
+//  ********************* string in js ************************
+
+// let name ="sandesh ";
+// let rollno =21;
+// let schoolname = " murari intercollege college";
+ 
+//add all string 
+
+// let allingo =name+rollno+schoolname;
+// console.log (allingo)
+
+//2nd  (string interpulation) used allways 
+// let allstr=`${name} ${rollno} ${schoolname}`
+// console.log(allstr)
+
+//declare str as object ,are containg prototype 
+ 
+// let gamenum=new  String ("sahjanwa") 
+// console.log (gamenum)
+// console.log(typeof gamenum)
+// console.log (gamenum.length)
+// console.log (gamenum.toUpperCase())
+// console.log(gamenum.charAt(5))
+
+
+// slice /substring and splice 
+// console.log (gamenum.substring(1,4))//work just like slice 
+// console.log (gamenum.slice(1,4))
+//different in slice and substring just is slice support negative num but substrng is not 
+
+// console.log (gamenum.splice(1,4))???
+
+// let practicename =" sahjanwa  ";        //trim ,trim start or end 
+// console.log (practicename)
+// console.log (practicename.length)
+
+// console.log (practicename.trim())
+// console.log (practicename.trim().length)
+
+// console.log (practicename.trimStart())
+// console.log (practicename.trimStart().length)
+
+// console.log (practicename.trimEnd())
+// console.log (practicename.trimEnd().length)
+
+
+
+//replace (find ,replacewith),replaceAll 
+
+// let repstr ="bhagwatgeetabhag";
+// console.log(repstr.replace("bha","ram"))//not change original array /value 
+// console.log (repstr)
+// console.log(repstr.replaceAll ("bha","ram"))
+// console.log (repstr)
+
+//include ,ans with boolinyun (true/false) ***** para-includes(searchString)
+
+// let SSTR ="gorkhpur";
+// console.log(SSTR.includes("khp")
+
+
+// *********** change str in arr *************
+//(1)- base on seprator 
+
+
+// let changestr ="s-andesh b-harti"
+// console.log (changestr.split( "-"))     
+
+// const gameName=new String("sandesh-bharti-kumar")
+// console.log (gameName.split("-"))
+//working both 
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// $$$$$$$$$$$$$$$$$        str by apana college $$$$$$$$$$$$$$$$$$$$$
+
+let str = "apana college "
+// console.log(str.length )
+console.log (str[1])
+//access str charactor by passing charactor index para- str[index]
+//template literals (string interpolation)
+//`string text ${expression} string text`
+
+
+//string method 
+//(1)-toUpperCase(), (2)-toLowerCase(), (3)-str.trim()[remolve all white space from strat and end ] 
+//(4)- slice(start,end?)[return a part of string ]
+//(5)- str1.conscat(str2)[joins str2 with str1 ]
+//(6)- replace(searchval,newval)  (7)- str.charAt(idx)
+
