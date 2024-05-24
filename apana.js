ab13d39aa64557b6286fe96079e1c7182dd63c0f
Sandeshbharti <167829972+Sandeshbharti@users.noreply.github.com>
2024-05-24 19:48:52 +0000

    conditnal

---
+++ apana.js
@@ -0,0 +1,42 @@
+// Apana college * lecture 02-Oprator and conditional 
+// arithmetic oprator + , - , * , / => normal oprator 
+// %  modulush oprator =>remender wale bhaiye 
+//** exponnetiation => ghati hai 
+//  unary oprator [++] increament ,[--]decrement 
+// Assignment oprator [=] assign vaule right to let variable,[+=],[-=],[*=],[**=],[%=]
+//all assign value in right to left 
+// a+=b mean a=a+b 
+let x=3   //x=3+5=8
+// console.log (x+=5) //out =>8
+
+//comparison oprator [==][!=] normal comparison oprator (not compare data types )
+// [===][!==] striked  comparison oprator (compare data type also )
+//normal >, <, >=, <= 
+
+// logical oprator in js [&&] logical and [||]-logical or ,[!]--logical not 
+
+// condition stasemment [ if ]
+// if (conditon){
+//     do any wor
+// }
+
+// let num=5;
+// if(num>10){
+//     console.log (`that num is greater than 10`)
+// }else {
+//     console .log (`that num is not greater than 10`)
+// }
+let a=15
+if (a<5){
+    console.log ("do any work for condi1")
+}else if (a>100 ){
+    console.log ("do any work for condi2")
+}else if (a>20 ){
+    console.log ("do any work for condi3")
+}else {console.log ("not match any condition  last condi ")}
+
+
+
+
+
+
