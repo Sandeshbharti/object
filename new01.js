186e775e30df5215308959b73aa1098269c0493b
Sandeshbharti <167829972+Sandeshbharti@users.noreply.github.com>
2024-05-24 08:57:54 +0000

    start break and continue

---
+++ new01.js
@@ -0,0 +1,68 @@
+// nullish coalecing oprator (??) special design for null and undefined value data type 
+// const num= null?? "sandesh";    //sandesh 
+// console.log(num)
+// const nem2= ""??42;               //empty str 
+// console.log(nem2)
+// const num2=10??0;           
+// console.log(num2)                  //10
+
+
+// let oneobj={}
+// let two ={
+// name:"sandesh bj",
+    // class :10,
+    // rollno:101
+// }
+  
+// how to check a object was empty or not 
+
+// let newbj=Object.keys(oneobj).length===0? console.log("true empty ") : console.log("no empty ")   //create new array  of provided objects keys  
+// console.log (newbj)
+
+// if (Object.keys(oneobj).length===0){
+//     console.log ("that obj was empty")
+// }else {
+//    console.log( "that obj contains datas ")
+// }
+
+// loops in js (nexted loops in js)
+
+// for (let i=1;i<=5;i++){
+//     console.log ("ii= ", i)
+//    for(let  j=1;j<=5;j++){
+//         console.log ("jjjj= ",j)
+//         console.log ("QQQ= ", i)
+        
+//     }
+// }
+// for(let  j=1;j<=5;j++){
+//     console.log ("j = ", j)
+// }
+
+// breack and continue 
+
+
+for (let i=1;i<=10;i++){
+    console.log ("loop was running =i ",i);
+    
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
