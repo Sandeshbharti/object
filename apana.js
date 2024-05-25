58d8c15ee2ef454afc5f0d9e2d9695a8d27730a2
Sandeshbharti <167829972+Sandeshbharti@users.noreply.github.com>
2024-05-25 04:44:07 +0000

    arary finished

--- apana.js
+++ apana.js
@@ -6,7 +6,7 @@
 // Assignment oprator [=] assign vaule right to let variable,[+=],[-=],[*=],[**=],[%=]
 //all assign value in right to left 
 // a+=b mean a=a+b 
-let x=3   //x=3+5=8
+// let x=3   //x=3+5=8
 // console.log (x+=5) //out =>8
 
 //comparison oprator [==][!=] normal comparison oprator (not compare data types )
@@ -101,20 +101,20 @@ let x=3   //x=3+5=8
 //********************************************************************************************** */
 //                        String in jawaScript 
 //****************************************************************************************** */
-let str ="SANdesh "
-let str2="BHarti"
+// let str ="SANdesh "
+// let str2="BHarti"
 // console.log (str.charAt(0))
 // console.log (str.length)
 // console.log (str.toUpperCase())
 // console.log (str.toLowerCase());
 
-let str3="  kumar   "
+// let str3="  kumar   "
 // console.log (str3)
 // console.log (str3.trim())
 
 // console.log (str.concat(str2,str3))
 
-let str4="aabbaaccddbb" 
+// let str4="aabbaaccddbb" 
 // console.log(str4.replace("bb","xx"))
 // console.log(str4.replaceAll ("bb","xx"))
 // console.log (str4.charAt(0))
@@ -123,6 +123,70 @@ let str4="aabbaaccddbb"
 // console.log(str4.slice(2))
 
 // console.log(...str,...str2,...str3)
+
+// console.log(str4.split(""))
+
+
+// *************************   Array in js *************************************
+
+// let arr=[11,22,3344,55,66,77,88]
+// console.log (arr.length)
+
+//looping on array in js with apana college 
+
+// let hero=["raja","ram","jai","mohan","radhe"]
+
+// for (let i=0;i<=hero.length;i++){
+    // console.log( hero[i])                  //give normaly only key 
+// }
+
+// for (val of hero){
+    // console.log (val)
+// }
+// for (val in hero ){
+//     console.log (hero[val])
+// }                                            //give normaly only key 
+
+
+
+let price =[250,645,300,900,50]        //offer 10% 
+// let pri = price[0]
+// let offer=price[0]/10
+// let priceofoff=pri-offer
+// for (let i=0;i<price.length;i++){
+//    let pri = price[i]
+// let offer=price[i]/10
+// let priceofoff=pri-offer
+//    console.log(`net price ${price[i]} offer is ${price[i]/10} final price ${pri-offer} `)
+// }
+
+// // log average of arary price 
+//  let ave =0;
+// for (val of price ){
+//      ave =ave+val;
+// }
+// // console.log (ave )
+
+// // array method in jawascript 
+// let arrin=[10,30,40,50]
+// arrin.push("bharti")
+// // console.log (arrin )
+// arrin.pop ()
+// // console.log (arrin )
+// arrin.unshift("bharti")
+// console.log (arrin )
+// arrin.shift ()
+// console.log (arrin )
+   
+let arrin=[10,30,40,50]
+// arrin.toString
+// console.log (typeof arrin.toString() )  // not changes original array 
+
+// console.log (arrin )
+// console.log ( typeof arrin )
+
+// console.log(arrin.splice(1,2,"sandesh"))  //not work proper 
+
 
 
 
